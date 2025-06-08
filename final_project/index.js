bf19bfcef2853ffe8ff5c6085a766c1fcf77581f
rakhikrishna321 <rakhikrishnagk@gmail.com>
2025-06-08 07:50:19 -0400

    Your task message here

--- final_project/index.js
+++ final_project/index.js
@@ -11,6 +11,19 @@ app.use(express.json());
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
 app.use("/customer/auth/*", function auth(req,res,next){
+    if(req.session && req.session.authorization){
+        const token = req.session.authorization.accessToken;
+        jwt.verify(token,"access",(err,user) =>{
+            if(err){
+                return res.status(403).json({message:"Invalid token"});
+            }
+            req.user = user;
+            next();
+        });
+
+    }else{
+        return res.status(403).json({message:"user not authenticated"})
+    }
 //Write the authenication mechanism here
 });
  
